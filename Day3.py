9fde7c22d51bd679dd84606ee6b3030fe1ec5aad
Midjord <midjord.andreas@gmail.com>
2020-12-03 06:59:16 +0100

    Dag 3: Trætælleren

---
+++ Day3.py
@@ -0,0 +1,26 @@
+import math
+from helpers import AoCHelper
+
+input = AoCHelper.readInputLines("day3/day3test0.txt")
+
+numberOfTrees = 1
+forest = []
+
+patternExpansion = math.ceil(len(input) / len(input[0]))
+
+for i in input:
+    forest.append(i*patternExpansion*10)
+
+def treesInSlope(x,y):
+    treesInSlope = 0
+    for k in range(math.ceil(len(input)/x)):
+        if forest[x*k][y*k] == '#':
+            treesInSlope+=1
+    return treesInSlope
+
+slopes = [[1,1], [1, 3], [1, 5], [1,7], [2,1]]
+
+for slope in slopes:
+    numberOfTrees*=treesInSlope(slope[0], slope[1])
+
+print(str(numberOfTrees))
