0651756e78f5ddee4f5d9f0687adb3398ab5bd05
Midjord <midjord.andreas@gmail.com>
2020-12-06 13:35:47 +0100

    Putting slopes-list into line and better variable names

--- Day3.py
+++ Day3.py
@@ -15,8 +15,7 @@ def treesInSlope(x,y):
 assert treesInSlope(1, 3) == 181
 print("Part 1: " + str(treesInSlope(1, 3)))
 
-slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
-numberOfTrees = prod([treesInSlope(slope[0], slope[1]) for slope in slopes])
+numberOfTrees = prod([treesInSlope(x, y) for x, y in [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]])
 
 assert numberOfTrees == 1260601650
 print("Part 2: " + str(numberOfTrees))
