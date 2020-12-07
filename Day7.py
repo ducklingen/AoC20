059b70475131bd78fe641c148ceb4c7c9ae2dac5
Midjord <midjord.andreas@gmail.com>
2020-12-07 07:07:02 +0100

    Day 7: Bag counting

---
+++ Day7.py
@@ -0,0 +1,58 @@
+from helpers.AoCHelper import *
+
+inputlines = readInputLines('day7/day7input1.txt')
+
+specialbag = 'shiny gold bag'
+
+bags = {}
+bagsWithShinyGold = 0
+
+for i in inputlines:
+    i = i.replace('bags', 'bag').replace('.', '')
+    container, indhold = i.split(' contain ')
+    indholdsliste = []
+    for j in indhold.split(', '):
+        if j == 'no other bag':
+            break
+
+        list = j.split(' ')
+        indholdsliste.append((list[0], listToString(list[1:], ' ').strip()))
+
+    bags[container] = indholdsliste
+
+
+def containsShinyGold(baglist, bags, parentbags):
+    contains = False
+    for bag in baglist:
+        if bag[1] == specialbag:
+            print(listToString(parentbags, ' -> ') + specialbag)
+            # print(parentbags[-1:])
+            contains = True
+            break
+
+    if not contains:
+        for bag in baglist:
+            parentage = parentbags
+            parentage.append(bag[1])
+            contains = containsShinyGold(bags[bag[1]], bags, parentage)
+            if contains:
+                break
+
+    return contains
+
+def containsNumberOfBags(baglist, bags):
+    bagsContained = 0
+    for bag in baglist:
+        print(bag[0] + ' x ' + bag[1])
+        bagsContained += int(bag[0]) + int(bag[0])*containsNumberOfBags(bags[bag[1]], bags)
+
+    return bagsContained
+
+
+# for bag in bags:
+#     if containsShinyGold(bags[bag], bags, [bag]):
+#         # print(bag)
+#         bagsWithShinyGold += 1
+
+print(bagsWithShinyGold)
+print(containsNumberOfBags(bags[specialbag], bags))
--- playground.py
+++ playground.py
@@ -1,1 +1,13 @@
-print(str(sum([1,2,3])))
+def sum(a, b):
+    print(str(a+b))
+
+sum(*[1, 2])
+
+print(str(123 & 456))
+
+dict = {'a': 1, 'b': 2}
+
+if 'c' in dict.keys():
+    print('Nop')
+else:
+    print('Jep')
