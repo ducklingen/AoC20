628b6635f2e19bacc789eba3d44be7b97f3face1
Andreas Midjord <anm@netcompany.com>
2020-12-08 08:36:24 +0100

    Cleaning up day 8

--- Day8.py
+++ Day8.py
@@ -1,57 +1,51 @@
 from helpers.AoCHelper import *
 
+inputlines = readInputLines('day8/day8input1.txt')
 
 def alterProgram(i, program):
     newProgram = [x for x in program]
     operation = program[i]
 
     if operation[0:3] == 'jmp':
-        print("Changed position " + str(i))
         newProgram[i] = operation.replace('jmp', 'nop')
     elif operation[0:3] == 'nop':
-        print("Changed position " + str(i))
         newProgram[i] = operation.replace('nop', 'jmp')
 
     return newProgram
 
-def executeOperation(i, program, accumulator):
-    if i >= len(program):
-        print("Out of index")
-        return accumulator, i
-
-    operation = program[i]
-    # print(operation)
-
+def executeOperation(i, operation, accumulator):
     if operation[0:3] == 'acc':
-        value = extract_numbers_from_line(operation)
-        accumulator += value[0]
+        accumulator += extract_numbers_from_line(operation)[0]
         i += 1
     elif operation[0:3] == 'jmp':
-        value = extract_numbers_from_line(operation)
-        i += value[0]
+        i += extract_numbers_from_line(operation)[0]
     else:
         i += 1
 
-    if i == len(program) and operation[0:3] in ['acc', 'nop']:
-        print('Program terminates')
-        i -= 1
-
     return accumulator, i
 
-def runprogram(currentLine, visitedLines, accumulator, program):
-    while currentLine not in visitedLines:
-        visitedLines.append(currentLine)
-        accumulator, currentLine = executeOperation(currentLine, program, accumulator)
 
-    return accumulator
+def runprogram(program):
+    current_line, accumulator, visited_lines, terminated = [0, 0, [], False]
 
+    while current_line not in visited_lines and current_line < len(program):
+        visited_lines.append(current_line)
+        accumulator, current_line = executeOperation(current_line, program[current_line], accumulator)
 
-inputlines = readInputLines('day8/day8input1.txt')
+    if current_line >= len(program):
+        terminated = True
 
+    return accumulator, terminated
+
+# Part 1
+assert runprogram(inputlines)[0] == 1744
+print("Part 1: " + str(runprogram(inputlines)[0]))
+
+# Part 2
 for i in range(len(inputlines)):
-    a = 0
-    c = 0
-    v = []
-    inputlines = readInputLines('day8/day8input1.txt')
-    alteredProgram = alterProgram(i, inputlines)
-    print(runprogram(c, v, a, alteredProgram))
+    a, terminated = runprogram(alterProgram(i, readInputLines('day8/day8input1.txt')))
+
+    if terminated:
+        assert a == 1174
+        print("Part 2: " + str(a))
+        break
