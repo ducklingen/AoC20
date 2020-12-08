a28944f642adac1c6b3e5cd4f30162e2fa975254
Andreas Midjord <anm@netcompany.com>
2020-12-08 08:50:33 +0100

    Formatting

--- Day8.py
+++ Day8.py
@@ -2,18 +2,20 @@ from helpers.AoCHelper import *
 
 inputlines = readInputLines('day8/day8input1.txt')
 
-def alterProgram(index, program):
-    newProgram = [x for x in program]
+
+def alter_program(index, program):
+    new_program = [x for x in program]
     operation = program[index]
 
     if operation[0:3] == 'jmp':
-        newProgram[index] = operation.replace('jmp', 'nop')
+        new_program[index] = operation.replace('jmp', 'nop')
     elif operation[0:3] == 'nop':
-        newProgram[index] = operation.replace('nop', 'jmp')
+        new_program[index] = operation.replace('nop', 'jmp')
 
-    return newProgram
+    return new_program
 
-def executeOperation(index, operation, accumulator):
+
+def execute_operation(index, operation, accumulator):
     if operation[0:3] == 'acc':
         accumulator += extract_numbers_from_line(operation)[0]
         index += 1
@@ -25,25 +27,26 @@ def executeOperation(index, operation, accumulator):
     return accumulator, index
 
 
-def runprogram(program):
-    current_line, accumulator, visited_lines, terminated = [0, 0, [], False]
+def run_program(program):
+    current_line, accumulator, visited_lines, terminated = [0, 0, set(), False]
 
     while current_line not in visited_lines and current_line < len(program):
-        visited_lines.append(current_line)
-        accumulator, current_line = executeOperation(current_line, program[current_line], accumulator)
+        visited_lines.add(current_line)
+        accumulator, current_line = execute_operation(current_line, program[current_line], accumulator)
 
     if current_line >= len(program):
         terminated = True
 
     return accumulator, terminated
 
+
 # Part 1
-assert runprogram(inputlines)[0] == 1744
-print("Part 1: " + str(runprogram(inputlines)[0]))
+assert run_program(inputlines)[0] == 1744
+print("Part 1: " + str(run_program(inputlines)[0]))
 
 # Part 2
 for i in range(len(inputlines)):
-    accumulator, terminated = runprogram(alterProgram(i, readInputLines('day8/day8input1.txt')))
+    accumulator, terminated = run_program(alter_program(i, readInputLines('day8/day8input1.txt')))
 
     if terminated:
         assert accumulator == 1174
